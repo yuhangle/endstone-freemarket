ecc06d983f65a04b14b9f5cf7e98c3b5e3ab6759
yuhangle <893033655@qq.com>
2025-06-18 11:58:07 +0800

    change version to 0.1.0,update endstone to 0.9.0

--- src/freemarket.cpp
+++ src/freemarket.cpp
@@ -4,7 +4,7 @@
 
 #include "freemarket.h"
 
-ENDSTONE_PLUGIN("freemarket", "0.1.0dev6", FreeMarket)
+ENDSTONE_PLUGIN("freemarket", "0.1.0", FreeMarket)
 {
     description = "An Endstone plugin for player to use in free market trading";
 
